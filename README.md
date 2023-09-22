66903896f950831ca77fc217bc3f0e087a32c05f
Andrea <andrea@kaist.ac.kr>
2023-09-22 16:31:24 +0900

    Init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Analysis of results from PCS
+
+Check how many papers match a PROMPT. Raw data is store in `data.json`
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+Place your OPENAI_API_KEY in a `.env` file.
+
+```toml
+OPENAI_API_KEY=sk-ozpZ...
+```
+
+To run:
+
+```bash
+bun start
+```
---
+++ index.ts
@@ -0,0 +1,123 @@
+// Constants
+const MAX_PER_QUERY = 100;
+const PAUSE = 30 * 1000;
+const PROMPT =
+  'Is this article about or related with artificial intelligence (AI), machine learning (ML), or large languag models (LLM)? Just reply "true" or "false" answer';
+
+// Start code from here
+import data from './data.json';
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// Types
+type Paper = {
+  id: number;
+  title: string;
+  abstract: string;
+};
+
+// Data processing
+const papers = data
+  .map((x): Paper => {
+    return {
+      id: parseInt(x[2] as any),
+      title: '' + x[3],
+      abstract: '' + x[9],
+    };
+  })
+  .sort((a, b) => {
+    return a.id - b.id;
+  });
+
+const totPapers = papers.length;
+
+// Main
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+const result: any = [];
+
+for (let i = 0; i < totPapers / MAX_PER_QUERY; i++) {
+  const min = i * MAX_PER_QUERY;
+  const tempMax = (i + 1) * MAX_PER_QUERY;
+  const max = tempMax < totPapers ? tempMax : totPapers;
+
+  console.log(
+    `Analyzing papers from ${min}[id: ${papers[min].id}] to ${max - 1}[id: ${
+      papers[max - 1].id
+    }]`
+  );
+
+  const annotateWitQuestion = await Promise.all(
+    papers.slice(min, max).map(async ({ id, title, abstract }) => {
+      const answer = await getExplanation(abstract, PROMPT);
+      return {
+        id,
+        title,
+        abstract,
+        matchQuery: answer?.toLowerCase(),
+      };
+    })
+  );
+  result.push(...annotateWitQuestion);
+  console.log('Waiting...');
+  await delay(PAUSE);
+}
+
+// Write to file
+
+const path = 'results.json';
+await Bun.write(path, JSON.stringify(result));
+
+// Analysis of results
+
+const matchQuery = result.filter((paper) => paper.matchQuery === 'true');
+
+const perc = ((100 * matchQuery.length) / result.length).toFixed(1);
+console.log(
+  `${matchQuery.length} papers out of ${result.length} matched the query (${perc}%)`
+);
+
+// Helpers
+async function getExplanation(input: string, question: string) {
+  try {
+    const stop = '###\n';
+    const prompt = input + stop + question;
+
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content: prompt,
+        },
+        {
+          role: 'assistant',
+          content: 'false',
+        },
+      ],
+      temperature: 1,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stop: ['###'],
+    });
+
+    const { content } = response.choices[0]?.message;
+
+    return content;
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status); // e.g. 401
+      console.log(error.response.data.message); // e.g. The authentication token you passed was invalid...
+      console.log(error.response.data.code); // e.g. 'invalid_api_key'
+      console.log(error.response.data.type); // e.g. 'invalid_request_error'
+    } else {
+      console.log(error);
+    }
+  }
+}
