cf164cbd6c979603a48e6c726d617aed6bfbbb63
Andrea <andrea@kaist.ac.kr>
2023-09-23 14:12:57 +0900

    Longer pause

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 // Constants
 const MAX_SIMULTANEOUS_QUERIES = 100;
-const PAUSE = 30 * 1000;
+const PAUSE = 60 * 1000;
 const PROMPT =
   'Is this paper discussing or using techniques based on artificial intelligence (AI), machine learning (ML), or large language models (LLMs)? Give a conservative answer to minimize false positives. Simply answer "true" or "false".';
 
